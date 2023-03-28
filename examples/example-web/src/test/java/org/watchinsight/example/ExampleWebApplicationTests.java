bfbf25016a781b0fe1d487c7e7979f822faa4b71
gerry <yuanguohua_7818@163.com>
2023-03-28 23:57:38 +0800

    Add example project

---
+++ examples/example-web/src/test/java/org/watchinsight/example/ExampleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.watchinsight.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
